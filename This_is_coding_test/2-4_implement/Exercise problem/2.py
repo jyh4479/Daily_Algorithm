bb701015eaf7369189c8e66bb8795de0eb8cddee
jyh4479 <jyh.dev@gmail.com>
2020-10-27 19:49:40 +0900

    Update chapter 2-4
    Update 2.py

--- This_is_coding_test/2-4_implement/Exercise problem/2.py
+++ This_is_coding_test/2-4_implement/Exercise problem/2.py
@@ -23,31 +23,32 @@ def move_back(cur_dir, cur_y, cur_x):
     if(cur_dir==0): #북
         cur_y+=1
         if(arr[cur_y][cur_x]==1):
-            return True
+            return True, cur_y, cur_x
         else:
-            return False
+            return False, cur_y, cur_x
     elif(cur_dir==1): #동
         cur_x-=1
         if(arr[cur_y][cur_x]==1):
-            return True
+            return True, cur_y, cur_x
         else:
-            return False
+            return False, cur_y, cur_x
     elif(cur_dir==2): #남
         cur_y-=1
         if(arr[cur_y][cur_x]==1):
-            return True
+            return True, cur_y, cur_x
         else:
-            return False
+            return False, cur_y, cur_x
     elif(cur_dir==3): #서
         cur_x+=1
         if(arr[cur_y][cur_x]==1):
-            return True
+            return True, cur_y, cur_x
         else:
-            return False
+            return False, cur_y, cur_x
 
 while(1):
     if(check_path()):
-        if(move_back(cur_dir, cur_y, cur_x)):
+        flag,cur_y,cur_x=move_back(cur_dir, cur_y, cur_x)
+        if(flag):
             print(cnt)
             break
 
@@ -58,4 +59,4 @@ while(1):
         cur_x+=dir_x[cur_dir]
         cnt+=1
         arr[cur_y][cur_x]=-1 #방문 체크
-        left(cur_dir)
+        cur_dir=left(cur_dir)
