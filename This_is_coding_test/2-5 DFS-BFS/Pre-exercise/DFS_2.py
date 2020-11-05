381bc3397c30db3b6b51086e8054764295830fb4
jyh4479 <jyh.dev@gmail.com>
2020-11-05 18:17:37 +0900

    Upload: chapter 2-5
    This_is_coding_test chapter2-5 upload

--- This_is_coding_test/2-5 DFS-BFS/Pre-exercise/DFS_2.py
+++ This_is_coding_test/2-5 DFS-BFS/Pre-exercise/DFS_2.py
@@ -11,6 +11,27 @@ graph=[
     ]
 visited=[False for _ in range(9)]
 
-"""
-stack으로 구현해보기
-"""
+stack=[]
+
+start_idx=1
+stack.append(start_idx)
+visited[start_idx]=True
+print(start_idx,end=" ")
+
+while(len(stack)):
+    v=stack[-1]
+
+    flag=0
+    for i in graph[v]:#방문하지 않은 인접 정점 탐색
+        if(visited[i]==False):#방문하지 않은경우
+            stack.append(i)
+            visited[i]=True
+            print(i,end=" ")
+            flag=1
+            break
+        elif(visited[i]==True):
+            continue
+    if(flag==0):
+        stack.pop()
+        #주위를 모두 방문했으면 stack삭제
+print()
