7a1ce787384de5cc030403a95e2f00166fb93d81
jyh4479 <jyh.dev@gmail.com>
2020-11-06 09:53:40 +0900

    Update: chapter 2-5
    This_is_coding_test DFS_2.py update

--- This_is_coding_test/2-5 DFS-BFS/Pre-exercise/DFS_2.py
+++ This_is_coding_test/2-5 DFS-BFS/Pre-exercise/DFS_2.py
@@ -31,3 +31,36 @@ while(len(stack)):
         if(i==graph[v][-1]):#인접 정점을 모두 방문한 경우
             stack.pop()
 print()
+
+
+"""
+graph=[
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+    ]
+visited=[False for _ in range(9)]
+stack=[]
+
+start_idx=1
+stack.append(start_idx)
+visited[start_idx]=True
+print(start_idx,end=" ")
+
+while stack:
+    v=stack.pop()
+    for i in graph[v]:
+        if not visited[i]:
+            stack.append(v)
+            stack.append(i)
+            visited[i]=True
+            print(i,end=" ")
+            break
+print()
+"""
--- This_is_coding_test/2-5 DFS-BFS/readme.txt
+++ This_is_coding_test/2-5 DFS-BFS/readme.txt
@@ -1,3 +1,3 @@
 stack사용에 대한 추가 설명 (참고: https://ooeunz.tistory.com/7)
 
-DFS_2 -> stack으로 구현한 DFS 좀더 간단하게 바꿔보기
+DFS_2 -> stack으로 구현한 DFS 좀더 간단하게 바꿔보기 (break가 중요함)
