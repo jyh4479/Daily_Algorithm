2005b7c978e213f8a9716b9a1eed24055d4a5e20
jyh4479 <jyh.dev@gmail.com>
2020-11-06 09:41:08 +0900

    Update: chapter 2-5
    This_is_coding_test DFS_1.py update

--- This_is_coding_test/2-5 DFS-BFS/Pre-exercise/DFS_1.py
+++ This_is_coding_test/2-5 DFS-BFS/Pre-exercise/DFS_1.py
@@ -11,18 +11,40 @@ graph=[
     ]
 visited=[False for _ in range(9)]
 
-
-#재귀로 구현
 def dfs(idx):
-    if(visited[idx]==False):
-        visited[idx]=True
-        print(idx,end=" ")
-    else:
-        for i in range(len(graph[idx])):
-            if(visited[graph[idx][i]]==False):
-                dfs(graph[idx][i])
-            else:
-                continue
+    visited[idx]=True
+    print(idx,end=" ")
+
+    for i in graph[idx]:
+        if not visited[i]:
+            dfs(i)
 
 dfs(1)
+
+""" 안보고 구현한 DFS
+graph=[
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+    ]
+visited=[False for _ in range(9)]
+
+def dfs(idx):
+    if visited[idx]==True:
+        return
+    else:
+        print(idx,end=" ")
+        visited[idx]=True
+        for i in graph[idx]:
+            dfs(i)
+
+start_idx=1
+dfs(start_idx)
 print()
+"""
