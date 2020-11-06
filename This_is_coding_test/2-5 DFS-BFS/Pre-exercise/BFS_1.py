8e388c8936bdc0e4878bfb413be897e741518075
jyh4479 <jyh.dev@gmail.com>
2020-11-06 12:49:50 +0900

    Update: chapter 2-5
    This_is_coding_test BFS_1.py update

--- This_is_coding_test/2-5 DFS-BFS/Pre-exercise/BFS_1.py
+++ This_is_coding_test/2-5 DFS-BFS/Pre-exercise/BFS_1.py
@@ -26,3 +26,34 @@ while q:
             q.append(i)
             visit[i]=True
 print()
+
+"""
+from collections import deque
+graph=[
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+    ]
+visited=[False for _ in range(9)]
+q=deque()
+
+start_idx=1
+q.append(start_idx)
+
+while q:
+    v=q.popleft() #방문
+    visited[v]=True
+    print(v,end=" ")
+
+    for i in graph[v]: #인접 정점 방문탐색
+        if not visited[i]:
+            visited[i]=True
+            q.append(i)
+print()
+"""
