2d848f9153e651ae962c4b2d8fd4f2635d9387c7
jyh4479 <jyh.dev@gmail.com>
2020-11-07 21:06:53 +0900

    Upload: chapter 2-5
    This_is_coding_test chapter2-5 upload

---
+++ This_is_coding_test/2-5 DFS-BFS/Exercise problem/2.py
@@ -0,0 +1,26 @@
+from collections import deque
+N,M=map(int,input().split())
+graph=[]
+for i in range(N):
+    graph.append(list(map(int,input()))) #표현방법 숙지
+
+dx=[1,-1,0,0]
+dy=[0,0,1,-1]
+start_i,start_j=0,0
+q=deque()
+q.append([start_i,start_j])
+
+while q:
+    v=q.popleft() #현재위치 불러오기
+
+    for i in range(4):
+        if v[0]+dy[i]<0 or v[1]+dx[i]<0 or v[0]+dy[i]>=N or v[1]+dx[i]>=M:
+            continue
+        if(graph[v[0]+dy[i]][v[1]+dx[i]]==0):
+            continue
+
+        if(graph[v[0]+dy[i]][v[1]+dx[i]]==1):   
+            graph[v[0]+dy[i]][v[1]+dx[i]]=graph[v[0]][v[1]]+1
+            q.append([v[0]+dy[i],v[1]+dx[i]]) #다음좌표 저장
+
+print(graph[N-1][M-1])
--- This_is_coding_test/2-5 DFS-BFS/readme.txt
+++ This_is_coding_test/2-5 DFS-BFS/readme.txt
@@ -1,3 +1,5 @@
 stack사용에 대한 추가 설명 (참고: https://ooeunz.tistory.com/7)
 
 DFS_2 -> stack으로 구현한 DFS 좀더 간단하게 바꿔보기 (break가 중요함)
+
+실전문제 제대로 이해하고 넘어가기 (최소값 출력 방법, 정보 입력받는 방법 등)
