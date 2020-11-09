82f43f8cfaddbcf53cadc3ea2e2f61eda6a986b7
jyh4479 <jyh.dev@gmail.com>
2020-11-09 13:08:41 +0900

    Upload: step 23
    BOJ step23 upload

---
+++ BOJ/python/23/7576.py
@@ -0,0 +1,38 @@
+from collections import deque
+M,N=map(int,input().split())
+graph=[]
+for _ in range(N): #밭 정보 받기
+    graph.append(list(map(int,input().split())))
+
+dx=[0,0,1,-1]
+dy=[1,-1,0,0]
+
+q=deque()
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 1: #토마토 찾기
+            q.append([i,j])
+
+while q:
+    v=q.popleft()
+    for i in range(4):
+        nx=v[1]+dx[i]
+        ny=v[0]+dy[i]
+        if nx<0 or ny<0 or nx>=M or ny>=N:
+            continue
+        if graph[ny][nx]==0: #안익은 토마토인경우
+            graph[ny][nx]=graph[v[0]][v[1]]+1
+            q.append([ny,nx])
+
+ans=-1
+breaker=0
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 0:
+            ans,breaker=0,1
+            break
+        ans=max(ans,graph[i][j])
+    if breaker==1:
+        break
+
+print(ans-1)
