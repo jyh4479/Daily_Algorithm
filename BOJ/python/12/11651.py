6e99aea5dbc2e9eabef03a2581270cfd1e7c952a
jyh4479 <jyh.dev@gmail.com>
2020-11-12 17:33:52 +0900

    Update: step 12
    BOJ step12 update

--- BOJ/python/12/11651.py
+++ BOJ/python/12/11651.py
@@ -6,3 +6,13 @@ for _ in range(num):
 num_list.sort(key=lambda x: (x[1],x[0]))
 for i in num_list:
     print(i[0],i[1])
+
+"""실패한 코드
+num=int(input())
+num_list=[]
+for _ in range(num):
+    num_list.append(list(map(int,input().split())))
+num_list.sort(key=lambda x: (x[1],x[0]))
+for i in num_list:
+    print(i[0],i[1])
+"""
