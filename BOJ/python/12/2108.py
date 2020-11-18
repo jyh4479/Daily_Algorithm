4e6abb6d68c24e6713078fca60e65873f1a2f55d
jyh4479 <jyh.dev@gmail.com>
2020-11-18 15:54:01 +0900

    Update: step 12
    BOJ step12 update

--- BOJ/python/12/2108.py
+++ BOJ/python/12/2108.py
@@ -10,7 +10,7 @@ def count(num_list):
         count_list[i]+=1
     ans=sorted(count_list, key=lambda x:(-count_list[x],x)) #sorted를 통해 list로 변경
 
-    if num==1:
+    if len(ans)==1: #-->num이 1인 경우로 조건을 주면 num은 2, 최빈값 리스트에서 len이 1인경우 오류가 남 (ex 2 -1 -1 입력시)
         return ans[0]
     else:
         if count_list[ans[0]] == count_list[ans[1]]:
