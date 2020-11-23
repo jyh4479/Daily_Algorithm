d4727a28925ae855b30f9da4b036df7e71493185
jyh4479 <jyh.dev@gmail.com>
2020-11-23 21:06:43 +0900

    Update: step 15
    BOJ step15 update

--- BOJ/python/15/1931.py
+++ BOJ/python/15/1931.py
@@ -1,9 +1,27 @@
+import sys
+N=int(input())
+range_list=list()
+for _ in range(N):
+    range_list.append(list(map(int,sys.stdin.readline().split())))
+
+range_list.sort(key=lambda x:(x[1],x[0]))
+#print(range_list)
+
+cnt=0
+end_time=0
+for time in range_list:
+    if end_time<=time[0]: #회의가 끝나고 시작할 수 있는 경우
+        end_time=time[1]
+        cnt+=1
+print(cnt)
+
+"""정렬 조건 체크 못함
 N=int(input())
 range_list=list()
 for _ in range(N):
     range_list.append(list(map(int,input().split())))
 
-range_list.sort(key=lambda x:(x[1]))
+range_list.sort(key=lambda x:(x[1])) #-->끝나는 시간이 같은경우 시작 시작도 정렬해줘야함
 
 cnt=0
 end_time=0
@@ -20,3 +38,4 @@ for time in range_list:
             cnt+=1
             continue
 print(cnt)
+"""
