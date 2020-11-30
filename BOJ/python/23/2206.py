1d0e17abdcb839baf973045c45b327e81908da4b
jyh4479 <jyh.dev@gmail.com>
2020-11-30 18:52:58 +0900

    Update: step 2206
    BOJ step2206 update

--- BOJ/python/23/2206.py
+++ BOJ/python/23/2206.py
@@ -2,7 +2,7 @@ import sys
 from collections import deque
 N,M=map(int,sys.stdin.readline().split())
 graph=[list(map(int,sys.stdin.readline().strip())) for _ in range(N)]
-move_graph=[[0 for _ in range(M)]for _ in range(N)]
+move_graph=[[[0 for _ in range(M)]for _ in range(N)]for _ in range(2)]
 q=deque()
 
 start_y=0
@@ -11,30 +11,35 @@ start_ability=1
 dy=[0,0,1,-1]
 dx=[1,-1,0,0]
 
+
 q.append([start_y,start_x,start_ability]) #시작점
+ans=0
 while q:
     v=q.popleft()
     y,x,ability=v[0],v[1],v[2]
+
+    if y==N-1 and x==M-1:
+        ans=move_graph[ability][y][x]+1
+        break
+   
     for i in range(4):
         ny=y+dy[i] #다음 위치 정의
         nx=x+dx[i]
+        nability=ability
+
         if ny<0 or nx<0 or ny>=N or nx>=M:
             continue
-        if graph[ny][nx]!=1: #벽이 아닌경우
-            move_graph[ny][nx]=move_graph[y][x]+1
-            q.append([ny,nx,ability]) #다음 방문 위치 저장
-            if ny==N-1 and nx==M-1:
-                q.clear()
-                break
-        elif graph[ny][nx]==1 and ability==1:
-            ability=0
-            move_graph[ny][nx]=move_graph[y][x]+1
-            q.append([ny,nx,ability]) #다음 방문 위치 저장
-            if ny==N-1 and nx==M-1:
-                q.clear()
-                break
 
-if move_graph[ny][nx]==0:
-    print(-1)
+        elif graph[ny][nx]==1 and nability==1: #벽이고 능력이 있는경우
+            nability=0
+            move_graph[0][ny][nx]=move_graph[1][y][x]+1
+            q.append([ny,nx,nability]) #다음 방문 위치 저장
+
+        elif graph[ny][nx]==0 and move_graph[nability][ny][nx]==0:
+            move_graph[nability][ny][nx]=move_graph[nability][y][x]+1
+            q.append([ny,nx,nability]) #다음 방문 위치 저장
+
+if ans!=0:
+    print(ans)
 else:
-    print(move_graph[ny][nx]+1)
+    print(-1)
