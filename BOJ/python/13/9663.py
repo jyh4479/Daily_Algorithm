560a4515b2f570873d10447bf92f90a38dfc8362
jyh4479 <jyh.dev@gmail.com>
2020-12-01 18:42:51 +0900

    Update: step 13
    BOJ step13 update

--- BOJ/python/13/9663.py
+++ BOJ/python/13/9663.py
@@ -1,3 +1,44 @@
+""" 조금 개선하긴 했지만 그래도 시간초과가 난다 (인터넷에서 사용하는 대각선을 따로 1차원 배열로 계산해서 풀어야한다)
+import sys
+N=int(sys.stdin.readline())
+graph=[[0 for _ in range(N)]for _ in range(N)]
+ans=0
+
+def check(i,j):
+    for x in range(N):
+        if graph[x][j]:
+            return False
+    ny,nx=i-1,j-1
+    while(ny>=0 and nx>=0):
+        if graph[ny][nx]:
+            return False
+        ny-=1
+        nx-=1
+
+    ny,nx=i-1,j+1
+    while(ny>=0 and nx<N):
+        if graph[ny][nx]:
+            return False
+        ny-=1
+        nx+=1
+    return True
+
+
+def dfs(cnt):
+    if cnt==N:
+        global ans
+        ans+=1
+        return
+    else:
+        for x in range(N):
+            if graph[cnt][x]!=1 and check(cnt,x)==True:
+                graph[cnt][x]=1 #체스 놓기
+                dfs(cnt+1)
+                graph[cnt][x]=0 #체스 회수
+dfs(0)
+print(ans)
+"""
+
 """시간 초과 나는 코드
 N=int(input())
 map=[[0 for _ in range(N)] for _ in range(N)]
