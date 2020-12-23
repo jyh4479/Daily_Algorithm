7b6e634ce0ba6d8404e5ea0238933bf832a33bf6
jyh4479 <jyh.dev@gmail.com>
2020-12-23 17:18:34 +0900

    Upload: kakao
    upload

---
+++ Programmers/2018kakao/[1차]뉴스클러스터링(오답).py
@@ -0,0 +1,39 @@
+def sum_set(A,B):
+    A_list,B_list=list(A),list(B)
+    for i in A_list:
+        if i in B_list:
+            B_list.remove(i)
+    print(A_list+B_list)
+    return len(A_list+B_list)
+
+def inter_set(A,B):
+    inter_list=[]
+
+    if len(A)>len(B):
+        A,B=B,A
+
+    for i in A:
+        if i in B:
+            inter_list.append(i)
+    print(inter_list)
+    return len(inter_list)
+
+def multi_set(string):
+    ans_list=[]
+    for i in range(len(string)-1):
+        sub_string=string[i:i+2]
+        if sub_string[0]<'A' or sub_string[0]>'Z' or sub_string[1]<'A' or sub_string[1]>'Z':
+            continue
+        ans_list.append(sub_string)
+    return ans_list
+
+def solution(str1, str2):
+    A,B=multi_set(str1.upper()),multi_set(str2.upper())
+    C,D=sum_set(A,B),inter_set(A,B)
+ 
+    if C==0:
+        ans=1
+    else:
+        ans=D/C
+
+    return int(ans*65536)
