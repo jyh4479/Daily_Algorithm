eaf56fa81c31f5e9be4c39fe475e9c109f6a6bf0
jyh4479 <jyh.dev@gmail.com>
2021-01-02 02:21:42 +0900

    Upload: chapter 2-9
    This_is_coding_test chapter2-9 upload

---
+++ This_is_coding_test/2-10 Graph/Disjoint_set_Cycle.py
@@ -0,0 +1,39 @@
+def find_parent(parent,x):
+    if parent[x]!=x:
+        parent[x]=find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a=find_parent(parent,a)
+    b=find_parent(parent,b)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
+
+v,e=map(int,input().split())
+parent=[0]*(v+1)
+
+for i in range(1,v+1):
+    parent[i]=i
+
+cycle=False
+
+for i in range(e):
+    a,b=map(int,input().split())
+    if find_parent(parent,a)==find_parent(parent,b):
+        cycle=True
+        break
+    else:
+        union_parent(parent,a,b)
+if cycle==True:
+    print("Cycle발생")
+else:
+    print("Cycle없음")
+"""
+6 4
+1 4
+1 3
+3 4
+5 6
+"""
