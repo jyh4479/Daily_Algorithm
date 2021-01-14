f33a300f2c1486e0bc8369024b599b599fcf1a2d
jyh4479 <jyh.dev@gmail.com>
2021-01-15 00:45:14 +0900

    Update: step 21
    BOJ step21 update

--- BOJ/python/21/11279.py
+++ BOJ/python/21/11279.py
@@ -1,3 +1,17 @@
+import sys
+import heapq
+input=sys.stdin.readline
+N=int(input())
+num_list=[]
+for _ in range(N):
+    cmd=int(input())
+    if cmd==0:
+        if len(num_list)==0:
+            print(0)
+        else:
+            print(heapq.heappop(num_list)[1])
+    else:
+        heapq.heappush(num_list,(-cmd,cmd))
 
 """힙으로 해결안하면 시간초과
 import sys
