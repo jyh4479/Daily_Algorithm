4b393feac2ef81d682b654187b6a32b6d3f27719
jyh4479 <jyh.dev@gmail.com>
2021-02-25 17:50:53 +0900

    Update: greedy
    BOJ greedy update

--- BOJ/python_greedy/1946.py
+++ BOJ/python_greedy/1946.py
@@ -1,3 +1,27 @@
+import sys
+num = int(sys.stdin.readline())
+
+for _ in range(num):
+    person_num = int(sys.stdin.readline())
+    array_score=[] 
+    for _ in range(person_num):
+        a,b = map(int,sys.stdin.readline().split())
+        array_score.append([a,b])
+
+    #algorithm logic
+    ans = 1
+    array_score = sorted(array_score, key=lambda x:x[0])
+    check = array_score[0][1]
+    for i in range(1,len(array_score)):
+        if check < array_score[i][1]:
+            continue
+        else:
+            ans+=1
+            check=array_score[i][1]
+
+    print(ans)
+     
+
 """시간초과
 import sys
 num = int(sys.stdin.readline())
