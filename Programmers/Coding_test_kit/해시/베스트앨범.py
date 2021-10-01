5621bfc241429377c16cbf6fb17293315a79c5d9
Jeong yong hoon <jyh.dev@gmail.com>
2021-10-02 00:13:05 +0900

    SOLVED

--- Programmers/Coding_test_kit/해시/베스트앨범.py
+++ Programmers/Coding_test_kit/해시/베스트앨범.py
@@ -9,9 +9,16 @@ def solution(genres, plays):
         if genre not in playList:
             playList[genre]=[0,[]]
         playList[genre][0]+=play
-        playList[genre][1].append((i,play))
+        playList[genre][1].append([i,play])
     
-    playList=sorted(playList.items(), key=lambda x:x[1][0])
-    print(playList)
-    answer = []
-    return answer
+    playList=sorted(playList.items(), key=lambda x:x[1][0], reverse=True)
+    
+    ans=[]
+    for play in playList:
+        ansPlay=sorted(play[1][1], key=lambda x:x[1], reverse=True)
+
+        for i in range(len(ansPlay)):
+            if i==2:break
+            ans.append(ansPlay[i][0])
+
+    return ans
