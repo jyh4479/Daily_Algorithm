a49169475b4bbe107f628cc4cd31710032434807
Jeong yong hoon <jyh.dev@gmail.com>
2021-10-13 23:37:10 +0900

    JAVA CODING TEST
    스터디 시작 전 각종 자료구조 다뤄보기

--- JavaCodingTest/src/com/company/Main.java
+++ JavaCodingTest/src/com/company/Main.java
@@ -8,5 +8,24 @@ public class Main {
         for (int i = 0; i < 5; i++) {
             System.out.println(i+" hello");
         }
+        
+        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> s = new Stack<>();
+        Deque<Integer> d = new LinkedList<>();
+
+        for(int i=0;i<9;i++){
+            q.add(i);
+            s.add(i);
+        }
+
+        while(!q.isEmpty()){
+            System.out.println(q.poll());
+        }
+        System.out.println(q);
+
+        while(!s.empty()){
+            System.out.println(s.pop());
+        }
+        System.out.println(s);
     }
 }
