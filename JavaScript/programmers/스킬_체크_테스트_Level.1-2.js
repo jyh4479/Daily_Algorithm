b1e394aa27eb1dbbb849e0777ef5f08030c6a790
jyh4479 <jyh.dev@gmail.com>
2022-08-25 20:43:54 +0900

    UPLOAD SOLVED

--- JavaScript/programmers/스킬_체크_테스트_Level.1-2.js
+++ JavaScript/programmers/스킬_체크_테스트_Level.1-2.js
@@ -1,1 +1,10 @@
-//이거 다시풀기
+function solution(x) {
+    const changeNumber = String(x);
+    let sum = 0;
+
+    for (let i = 0; i < changeNumber.length; i++) {
+        sum += Number(changeNumber[i]);
+    }
+
+    return x % sum === 0;
+}
