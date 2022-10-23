60bdbb0deeda02e3c6aa9035dc68e42ad2bd33e8
jyh4479 <jyh.dev@gmail.com>
2022-10-23 13:57:28 +0900

    UPDATE DFS, BFS
    js로 재귀를통한 DFS 구현하기

---
+++ JavaScript/basic/bfs.js
@@ -0,0 +1,42 @@
+const graph = {
+    A: ["B", "C"],
+    B: ["A", "D"],
+    C: ["A", "G", "H", "I"],
+    D: ["B", "E", "F"],
+    E: ["D"],
+    F: ["D"],
+    G: ["C"],
+    H: ["C"],
+    I: ["C", "J"],
+    J: ["I"]
+};
+
+// queue를 이용한 dfs, bfs
+/*
+
+원래 stack, queue를 각각 이용해 dfs, bfs를 구현하지만
+js에서는 구조 분해 할당을 통해 자료구조 한가지를 통해 모두 구현할 수 있다.
+
+ */
+const bfs = (start) => {
+    const visited = [];
+    let needVisit = [];
+
+    needVisit.push(start);
+
+    while (needVisit.length > 0) {
+        const node = needVisit.shift();
+
+        if (!visited.includes(node)) {
+            visited.push(node);
+
+            //구조분해 할당이 일어나는 부분
+            needVisit = [...needVisit, ...graph[node]];
+
+        }
+    }
+
+    return visited;
+}
+
+console.log(bfs("A"));
--- JavaScript/basic/dfs.js
+++ JavaScript/basic/dfs.js
@@ -11,7 +11,13 @@ const graph = {
     J: ["I"]
 };
 
-// stack을 이용한 dfs
+// queue를 이용한 dfs, bfs
+/*
+
+원래 stack, queue를 각각 이용해 dfs, bfs를 구현하지만
+js에서는 구조 분해 할당을 통해 자료구조 한가지를 통해 모두 구현할 수 있다.
+
+ */
 const dfs = (start) => {
     const visited = [];
     let needVisit = [];
@@ -23,7 +29,10 @@ const dfs = (start) => {
 
         if (!visited.includes(node)) {
             visited.push(node);
+
+            //구조분해 할당이 일어나는 부분
             needVisit = [...graph[node], ...needVisit];
+
         }
     }
 
