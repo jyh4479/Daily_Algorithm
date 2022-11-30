1f90eb3afc5abfc2cee0df67c315970e1ae449f8
jyh4479 <jyh.dev@gmail.com>
2022-11-30 22:21:54 +0900

    UPLOAD LEETCODE SOLVED

---
+++ LeetCode/1207.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    const resultObject = {};
+    const checkObject = {};
+
+    arr.forEach(data=>{
+        if(resultObject[data]===undefined){
+            resultObject[data]=1;
+        } else {
+            resultObject[data]++;
+        }
+    })
+
+
+    for(let key in resultObject){
+        if(checkObject[resultObject[key]]===undefined){
+            checkObject[resultObject[key]]=true;
+        } else {
+            return false;
+        }
+    }
+    return true;
+};
