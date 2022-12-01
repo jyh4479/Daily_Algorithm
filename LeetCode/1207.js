ff8b3f07551cf8527875baaed0fe48f8461b5655
jyh4479 <jyh.dev@gmail.com>
2022-12-01 21:36:12 +0900

    UPDATE FORMAT

--- LeetCode/1207.js
+++ LeetCode/1207.js
@@ -2,22 +2,22 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var uniqueOccurrences = function(arr) {
+var uniqueOccurrences = function (arr) {
     const resultObject = {};
     const checkObject = {};
 
-    arr.forEach(data=>{
-        if(resultObject[data]===undefined){
-            resultObject[data]=1;
+    arr.forEach(data => {
+        if (resultObject[data] === undefined) {
+            resultObject[data] = 1;
         } else {
             resultObject[data]++;
         }
     })
 
 
-    for(let key in resultObject){
-        if(checkObject[resultObject[key]]===undefined){
-            checkObject[resultObject[key]]=true;
+    for (let key in resultObject) {
+        if (checkObject[resultObject[key]] === undefined) {
+            checkObject[resultObject[key]] = true;
         } else {
             return false;
         }
