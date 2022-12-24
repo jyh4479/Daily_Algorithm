8aa67273b27530fb1fe26f63c766fc586565d56e
jyh4479 <jyh.dev@gmail.com>
2022-12-24 14:02:33 +0900

    UPLOAD SOLVED

---
+++ LeetCode/790-fail.js
@@ -0,0 +1,15 @@
+var numTilings = function (n) {
+    let md = 1e9;
+    md += 7;
+    const v = new Array(1001).fill(0);
+    v[1] = 1;
+    v[2] = 2;
+    v[3] = 5;
+    if (n <= 3)
+        return v[n];
+    for (let i = 4; i <= n; ++i) {
+        v[i] = 2 * v[i - 1] + v[i - 3];
+        v[i] %= md;
+    }
+    return v[n];
+};
