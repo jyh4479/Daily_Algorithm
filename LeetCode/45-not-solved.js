536e9a4c4b62b1f1c26f7455bd9d0689079ca043
jyh4479 <jyh.dev@gmail.com>
2023-02-08 19:25:29 +0900

    UPLOAD SOLVED

---
+++ LeetCode/45-not-solved.js
@@ -0,0 +1,14 @@
+var jump = function (nums) {
+
+    let jumpsRequired = 0, currenJumpEnd = 0, farthest = 0;
+
+    for (let g = 0; g < nums.length - 1; g++) {
+        farthest = Math.max(farthest, nums[g] + g);
+        if (g == currenJumpEnd) {
+            jumpsRequired++;
+            currenJumpEnd = farthest;
+        }
+    }
+
+    return jumpsRequired;
+};
