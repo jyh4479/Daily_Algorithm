8ea187bf70dcca97374e13fc56c3c5ad0b380a41
Jeong yong hoon <jyh.dev@gmail.com>
2023-03-13 10:44:05 +0900

    UPLOAD SOLVED

---
+++ LeetCode/101.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    const leftList = [];
+    const rightList =[];
+
+    const preOrder = (node) => {
+        if(node===null) {
+            leftList.push(null);
+            return;
+        } else {
+            leftList.push(node.val);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    const postOrder = (node) => {
+        if(node===null) {
+            rightList.push(null);
+            return;
+        } else {
+            rightList.push(node.val);
+            postOrder(node.right);
+            postOrder(node.left);
+        }
+    }
+
+    preOrder(root.left);
+    postOrder(root.right);
+
+    if(leftList.length!==rightList.length) return false;
+    for(let i=0;i<leftList.length;i++){
+        if(leftList[i]!==rightList[i]) return false;
+    }
+    return true;
+};
