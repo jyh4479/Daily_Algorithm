c14dd981cd4b6d1685b94803ed3c4b9e803b201e
Jeong yong hoon <jyh.dev@gmail.com>
2023-03-27 10:14:00 +0900

    UPLOAD SOLVED

---
+++ LeetCode/64.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+    //left, down, right
+    const dir = [[-1,0],[0,1],[1,0]];
+    const m = grid.length;
+    const n = grid.length===0 ? 0 : grid[0].length;
+    const visited = [];
+    for(let i=0;i<m;i++) visited.push(new Array(n).fill(false));
+    let ans = Number.MAX_VALUE;
+
+    const dfs = (y, x, cost) => {
+
+        if(y===(m-1) && x===(n-1)) {
+            ans = Math.min(ans, cost);
+            return;
+        }
+
+        for(let i=0;i<dir.length;i++){
+            const dy = y + dir[i][1];
+            const dx = x + dir[i][0];
+
+            if(dy < 0 || dx < 0 || dy >= m || dx >= n || visited[dy][dx]) continue;
+
+            visited[dy][dx]=true;
+            dfs(dy, dx, cost + grid[dy][dx]);
+            visited[dy][dx]=false;
+        }
+    }
+
+    visited[0][0]=true;
+    dfs(0,0,grid[0][0]);
+
+    return ans;
+};
