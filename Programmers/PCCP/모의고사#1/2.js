402cbd17b60c8a4559008462c3382ecb61adf958
jyh4479 <jyh.dev@gmail.com>
2023-10-24 14:36:54 +0900

    fix solution

--- Programmers/PCCP/모의고사#1/2.js
+++ Programmers/PCCP/모의고사#1/2.js
@@ -1,36 +1,61 @@
+// function solution(ability) {
+//
+//     const ansList = [];
+//     const selectedIndex = [];
+//
+//     const getScore = (personList) => {
+//         const selectedSports = [];
+//
+//         const scoreDfs = (index, sel, score) => {
+//             if (sel === personList.length) {
+//                 ansList.push(score);
+//             } else {
+//                 for (let i = 0; i < ability[personList[index]].length; i++) {
+//                     if (selectedSports.includes(i)) continue;
+//
+//                     selectedSports.push(i);
+//                     scoreDfs(index + 1, sel + 1, score + ability[personList[index]][i]);
+//                     selectedSports.pop();
+//                 }
+//             }
+//         }
+//
+//         scoreDfs(0, 0, 0);
+//     }
+//
+//     const dfs = (index, sel) => {
+//         if (sel === ability[0].length) {
+//             getScore(selectedIndex);
+//         } else {
+//             for (let i = index; i < ability.length; i++) {
+//                 selectedIndex.push(i);
+//                 dfs(i + 1, sel + 1);
+//                 selectedIndex.pop();
+//             }
+//         }
+//     }
+//
+//     dfs(0, 0);
+//
+//     ansList.sort((a, b) => Number(b) - Number(a));
+//
+//     return ansList[0];
+// }
+
 function solution(ability) {
-
     const ansList = [];
-    const selectedIndex = [];
+    const visited = new Array(ability.length).fill(false);
 
-    const getScore = (personList) => {
-        const selectedSports = [];
-
-        const scoreDfs = (index, sel, score) => {
-            if (sel === personList.length) {
-                ansList.push(score);
-            } else {
-                for (let i = 0; i < ability[personList[index]].length; i++) {
-                    if (selectedSports.includes(i)) continue;
-
-                    selectedSports.push(i);
-                    scoreDfs(index + 1, sel + 1, score + ability[personList[index]][i]);
-                    selectedSports.pop();
-                }
-            }
-        }
-
-        scoreDfs(0, 0, 0);
-    }
-
-    const dfs = (index, sel) => {
+    const dfs = (sel, sum) => {
         if (sel === ability[0].length) {
-            getScore(selectedIndex);
+            ansList.push(sum);
         } else {
-            for (let i = index; i < ability.length; i++) {
-                selectedIndex.push(i);
-                dfs(i + 1, sel + 1);
-                selectedIndex.pop();
+            for (let i = 0; i < ability.length; i++) {
+                if (visited[i]) continue;
+
+                visited[i] = true;
+                dfs(sel + 1, sum + ability[i][sel]);
+                visited[i] = false;
             }
         }
     }
