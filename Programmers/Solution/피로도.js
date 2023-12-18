1c9f84f036bd5b47a82717b39bdff3832c3dce62
Jeong yong hoon <jyh.dev@gmail.com>
2023-12-18 18:10:11 +0900

    upload code

--- Programmers/Solution/피로도.js
+++ Programmers/Solution/피로도.js
@@ -4,8 +4,6 @@ function solution(k, dungeons) {
     const visited = new Array(dungeons.length).fill(false);
     const dl = dungeons.length;
 
-    const numList = [];
-
     const dfs = (sel, energy) => {
 
         ans = Math.max(ans, sel);
@@ -13,9 +11,7 @@ function solution(k, dungeons) {
         for (let i = 0; i < dl; i++) {
             if (!visited[i] && energy >= dungeons[i][0]) {
                 visited[i] = true;
-                numList.push(i);
                 dfs(sel + 1, energy - dungeons[i][1]);
-                numList.pop();
                 visited[i] = false;
             }
         }
